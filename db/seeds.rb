b06a9cac947c3b5e38a31e453a1291a6a5b22e7a
Alfred Moore <ak_moore@live.com>
2017-11-20 11:12:08 -0600

    Seeded, Created PagesController, and Set root

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+breed_names = ['German Shephard', 'Basset Hound', 'Bull Dog', 'Dingo', 'Catahoula', 'Dalmation', 'Lab', 'Austrailian Shepard', 'Poodle', 'Maltese', 'Blue Healer', 'Mutt', 'Jack Russel Terrier', 'Pit Bull', 'Chihuaha', 'Golden Retriever', 'Terrier', 'Shi-Tzu', 'Chow', 'Greyhound', 'Bichon Frise']
+breed_names.each do |breed_name|
+    Breed.create(name: breed_name)
+end
+puts "Your Breed has been seeded"
+
